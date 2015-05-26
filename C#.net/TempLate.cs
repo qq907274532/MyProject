b72741214891e32b65331549c1398408c3c0b38e
xk <changxiaokang@hotmail.com>
2015-05-27 00:38:40 +0800

    娣诲姞妯℃澘瀹炵幇绀轰緥

---
+++ C#.net/TempLate.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TempLate
+{
+    class Program
+    {
+        static void Swap<T>(ref T lhs, ref T rhs)
+        {
+            T temp;
+            temp = lhs;
+            lhs = rhs;
+            rhs = temp;
+        }
+
+        static void Main(string[] args)
+        {
+            int a = 1;
+            int b = 2;
+
+            Swap<int>(ref a, ref b);
+        }
+    }
+}
+
