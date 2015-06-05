c774cc9cddcabc5121eef38b87f6453dacb17c11
xk <changxiaokang@hotmail.com>
2015-06-05 22:45:15 +0800

    update

---
+++ Python/docs/1.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+
+nNumA = int(raw_input("Pllease input nNumA : "))
+nNumB = int(raw_input("Pllease input nNumB : "))
+
+print "nNumA + nNumB = ", nNumA + nNumB
+print "nNumA - nNumB = ", nNumA - nNumB
+print "nNumA * nNumB = ", nNumA * nNumB
+print "nNumA / nNumB = ", nNumA / nNumB
