2bbc6c25df6f1226e15c4a3567fd4f2566ff483b
xk <changxiaokang@hotmail.com>
2015-06-12 12:30:36 +0800

    add strcpy

---
+++ C&C++/C/字符串操作/MyStrcpy.c
@@ -0,0 +1,34 @@
+#include <stdafx.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+char* MyStrCpy(char* szDst, const char* szSrc)
+{
+  char* szTemp = szDst;
+
+  while(*szTemp++ = *szSrc++)
+    ;
+
+  return szDst;
+}
+
+int main()
+{
+  char* szBuf = "changxiaokang";
+  char* szTemp = new char[13];
+  if(NULL == szTemp)
+  {
+    exit(-1);
+  }
+
+  MyStrCpy(szTemp, szBuf);
+  puts(szTemp);
+
+  if(szTemp)
+  {
+    delete szTemp;
+    szTemp = NULL;
+  }
+
+  return 0;
+}
