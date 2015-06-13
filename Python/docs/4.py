cbed74476ef41bf88d0f2730d33d81dae025feb6
xk <changxiaokang@hotmail.com>
2015-06-13 23:05:15 +0800

    update

--- Python/docs/4.py
+++ Python/docs/4.py
@@ -1,7 +1,20 @@
 #!/usr/bin/python
+#coding:utf-8
 
-def PutHello():
-	print "Hello, word!"
+def StrCmp(nX, nY = 100):
+	if nX == nY:
+		print nX, "=", nY
+	elif nX > nY:
+		print nX, ">", nY
+	elif nX < nY:
+		print nX, "<", nY
 
 
-PutHello()
+nNumA = raw_input("Plase input nNumA:")
+nNumB = raw_input("Plase input nNumB:")
+
+StrCmp(nNumA, nNumB)
+StrCmp(20)
+
+print "==================================="
+
---
+++ Python/src/Hello.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+print 20 + 80		# 打印 100 
+print 'Hello, word!'	# 打印 Hello, wrod!
+print '# 是Python 里面的注释'
+print '==============================='
+a = 123	# a是整数
+print a
+a = 'chang xiao kang'
+print a
+print '==============================='
+print 'Hello \r\n word!'	# 打印 Hello 换行 word!
+print 'Hello \t word!'		# 打印 Hello 制表符 word!
+print 'Hello \\ word!'		# 打印 Hello \ word!
+print '==============================='
+print r'\(~_~)/\(~_~)/'		# 打印 r表示这是一个raw字符串 \(~_~)/\(~_~)/
+print '显示中文'
+print '中文\n日文\n韩文'
+print '==============================='
+print '床前明月光'
+print '疑是地上霜'
+print '举头望明月'
+print '低头思故乡'
+
