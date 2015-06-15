6ede050888b97ed06da7b0bfead76b91a3a13966
xk <changxiaokang@hotmail.com>
2015-06-15 18:39:51 +0800

    update

--- Python/docs/4.py
+++ Python/docs/4.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 #coding:utf-8
 
+from __future__  import division
+
 def StrCmp(nX, nY = 100):
 	if nX == nY:
 		print nX, "=", nY
@@ -9,11 +11,7 @@ def StrCmp(nX, nY = 100):
 	elif nX < nY:
 		print nX, "<", nY
 
-
-nNumA = raw_input("Plase input nNumA:")
-nNumB = raw_input("Plase input nNumB:")
-
-StrCmp(nNumA, nNumB)
+StrCmp(25, 28)
 StrCmp(20)
 
 print "==================================="
@@ -23,3 +21,40 @@ def fun():
 fun()
 
 print "==================================="
+def Add(nX, nY):
+	return nX + nY
+
+def Sub(nX, nY):
+	return nX - nY
+
+def Mul(nX, nY):
+	return nX * nY
+
+def Div(nX, nY):
+	return nX / nY
+
+# def Operator(nX, o, nY):
+# 	if o == "+":
+# 		print Add(nX, nY)
+
+# 	elif o == "-":
+# 		print Sub(nX, nY)
+
+# 	elif o == "*":
+# 		print Mul(nX, nY)
+
+# 	elif o == "/":
+# 		print Div(nX, nY)
+
+# 	else:
+# 		pass
+
+Operator = {"+":Add, "-":Sub, "*":Mul, "/":Div}
+
+def Oper(nX, szOper, nY):
+	print Operator.get(szOper)(nX, nY)
+
+nNumA = int(raw_input("Plase input nNumA:"))
+nNumB = int(raw_input("Plase input nNumB:"))
+szOper = raw_input("Plase input operation method:")
+Oper(nNumA, szOper, nNumB)
